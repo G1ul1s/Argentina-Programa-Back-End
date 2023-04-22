326233a2ddb2002ce0181a00d4e67c9fc740a232
G1ul1s <giulianacervi@gmail.com>
2023-04-22 07:46:29 -0300

    CorsConfig

---
+++ src/main/java/com/finalentrega/portfoliofinal/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package com.finalentrega.portfoliofinal;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+      .allowedOrigins("https://argentina-programa-front-75022.web.app")
+      .allowedMethods("GET", "POST", "PUT", "DELETE")
+      .allowedHeaders("*");
+  }
+}
+
