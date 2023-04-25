9a08f6ca53f5342a9222f5199adfbbf444f7f768
G1ul1s <giulianacervi@gmail.com>
2023-04-25 06:52:24 -0300

    Allow

--- src/main/java/com/finalentrega/portfoliofinal/controlador/Controller.java
+++ src/main/java/com/finalentrega/portfoliofinal/controlador/Controller.java
@@ -11,15 +11,18 @@ import com.finalentrega.portfoliofinal.servicio.SEducacion;
 import com.finalentrega.portfoliofinal.servicio.PersonaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "https://argentina-programa-front-75022.web.app")
+@RequestMapping("/auth")
 @RestController
 public class Controller {
     
@@ -91,7 +94,7 @@ public class Controller {
         persoServ.borrarExperiencia(idexp,idper);
     }
        
-    //Certificaciones
+    //HyS
     @PostMapping("/new/hys/{id}")
     public void crearHyS(@RequestBody HyS hys, @PathVariable Persona id){
       persoServ.agregarHyS(hys, id);
