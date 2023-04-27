574447fd6380f0187942035c1300a101716485f0
G1ul1s <giulianacervi@gmail.com>
2023-04-27 13:10:38 -0300

    localhost iniciarsesion

--- src/main/java/com/finalentrega/portfoliofinal/PortfoliofinalApplication.java
+++ src/main/java/com/finalentrega/portfoliofinal/PortfoliofinalApplication.java
@@ -20,7 +20,7 @@ public class PortfoliofinalApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("https://frontend-giuli.web.app","http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+                registry.addMapping("/**").allowedOrigins("https://frontend-giuli.web.app","http://localhost:4200/iniciar-sesion").allowedMethods("*").allowedHeaders("*");
             }
         };
     }
--- src/main/java/com/finalentrega/portfoliofinal/controlador/Controller.java
+++ src/main/java/com/finalentrega/portfoliofinal/controlador/Controller.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = {"https://frontend-giuli.web.app", "http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontend-giuli.web.app", "http://localhost:4200/iniciar-sesion"})
 @RestController
 public class Controller {
     
