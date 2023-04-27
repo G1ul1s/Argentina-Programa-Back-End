f5ba81c68e00d0fc972c6c6a2eee5cc4ff234240
G1ul1s <giulianacervi@gmail.com>
2023-04-27 19:41:15 -0300

    cors

--- src/main/java/com/finalentrega/portfoliofinal/PortfoliofinalApplication.java
+++ src/main/java/com/finalentrega/portfoliofinal/PortfoliofinalApplication.java
@@ -2,9 +2,6 @@ package com.finalentrega.portfoliofinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfoliofinalApplication {
@@ -12,16 +9,5 @@ public class PortfoliofinalApplication {
 		public static void main(String[] args) {
 		SpringApplication.run(PortfoliofinalApplication.class, args);           
 }
-              
-//configurando el cors para la conexion con angular
-   @Bean
-
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("https://a-p-giuli-front-end.web.app/","http://localhost:4200").allowedMethods("*").allowedHeaders("*");
-            }
-        };
-    }
+          
 }                
