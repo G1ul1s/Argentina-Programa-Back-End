33f9f41d74fd44d376c4c9af25513cffbac78c3b
G1ul1s <giulianacervi@gmail.com>
2023-04-27 22:04:55 -0300

    cors headers

--- src/main/java/com/finalentrega/portfoliofinal/CorsConfiguration.java
+++ src/main/java/com/finalentrega/portfoliofinal/CorsConfiguration.java
@@ -1,18 +1,28 @@
 package com.finalentrega.portfoliofinal;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CorsConfiguration implements WebMvcConfigurer {
+public class CorsConfiguration {
 
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-      .allowedOrigins("https://a-p-giuli-front-end.web.app/", "http://localhost:4200")
-      .allowedMethods("GET", "POST", "PUT", "DELETE")
-      .allowedHeaders("*");
-  }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://a-p-giuli-front-end.web.app/", "http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("Content-Type", "Authorization")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+
+            }
+        };
+    }
 }
-
