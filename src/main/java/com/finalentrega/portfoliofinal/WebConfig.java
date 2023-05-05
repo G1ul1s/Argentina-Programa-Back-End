cbd4c70eee849b53fe86b6324db256f159f9f7a9
G1ul1s <giulianacervi@gmail.com>
2023-05-05 12:20:57 -0300

    webconfig

--- src/main/java/com/finalentrega/portfoliofinal/WebConfig.java
+++ src/main/java/com/finalentrega/portfoliofinal/WebConfig.java
@@ -5,13 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
-    public void addCorsMappings (CorsRegistry registry){
-        registry.addMapping("/");
-    
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*");
     }
-    
 }
